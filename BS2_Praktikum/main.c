4e88c647b6f4a8d7aee648c2f1cd30f86bbf322c
Markus Klemm <markus@markusklemm.net>
2015-04-08 13:40:24 +0200

    Initial Commit

---
+++ BS2_Praktikum/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BS2_Praktikum
+//
+//  Created by Markus Klemm on 08.04.15.
+//  Copyright (c) 2015 net.markusklemm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
